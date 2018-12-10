78409eaa1da63fae14d9c7848346c2154e739b92
coolutkarshraj <utkarshcpr@gmail.com>
2018-12-11 03:05:41 +0530

    bottom navigation added

--- app/src/main/java/com/io/utkarsh/cityguide/Activity/Splash/SplashActivity.java
+++ app/src/main/java/com/io/utkarsh/cityguide/Activity/Splash/SplashActivity.java
@@ -1,9 +1,12 @@
 package com.io.utkarsh.cityguide.Activity.Splash;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 import com.crashlytics.android.Crashlytics;
+import com.io.utkarsh.cityguide.Activity.Home.MainActivity;
 import com.io.utkarsh.cityguide.R;
 import io.fabric.sdk.android.Fabric;
 
@@ -11,13 +14,26 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        initView();
+
+    }
+
+    private void initView() {
         Fabric.with(this, new Crashlytics());
         final Fabric fabric = new Fabric.Builder(this)
                 .kits(new Crashlytics())
                 .debuggable(true)
                 .build();
         Fabric.with(fabric);
-        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
 
+            }
+        },5000);
     }
 }
