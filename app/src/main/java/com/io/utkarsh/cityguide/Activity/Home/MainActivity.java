bd6fe9a6fd7857466aa4edcb27220d11d950eb15
coolutkarshraj <utkarshcpr@gmail.com>
2018-12-18 02:15:11 +0530

    navigation drawer added

--- app/src/main/java/com/io/utkarsh/cityguide/Activity/Home/MainActivity.java
+++ app/src/main/java/com/io/utkarsh/cityguide/Activity/Home/MainActivity.java
@@ -1,15 +1,30 @@
 package com.io.utkarsh.cityguide.Activity.Home;
 
+import android.accounts.Account;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.io.utkarsh.cityguide.Fragment.AccountFragment;
+import com.io.utkarsh.cityguide.Fragment.MapViewFragment;
 import com.io.utkarsh.cityguide.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private ImageView ivOpenDrawer;
+    private DrawerLayout drawer;
+    Fragment currFrag;
+    AccountFragment accountFragment;
+    MapViewFragment mapViewFragment;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,10 +33,14 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
+                    mapViewFragment = new MapViewFragment();
+                    changeFrag(mapViewFragment,false);
                     return true;
                 case R.id.navigation_dashboard:
                     return true;
                 case R.id.navigation_notifications:
+                    accountFragment = new AccountFragment();
+                    changeFrag(accountFragment,false);
                     return true;
             }
             return false;
@@ -38,16 +57,76 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void bindListner() {
-
+        ivOpenDrawer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawer.openDrawer(GravityCompat.START);
+            }
+        });
     }
 
     private void startWorking() {
 
     }
 
-    private void initView() {
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    private void changeFrag(Fragment fragment, boolean addToBack) {
+
+        currFrag = fragment;
+        FragmentTransaction m = getSupportFragmentManager().beginTransaction()
+                .replace(R.id.content_view, fragment);
+        if (addToBack) {
+            m.addToBackStack(null);
+            m.setCustomAnimations(R.anim.fade_in,
+                    R.anim.fade_out);
+        }
+        m.commit();
     }
 
+    private void initView() {
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        ivOpenDrawer = findViewById(R.id.ivOpenDrawer);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main_drawer, menu);
+        return true;
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
---
+++ app/src/main/java/com/io/utkarsh/cityguide/Fragment/MapViewFragment.java
@@ -0,0 +1,45 @@
+package com.io.utkarsh.cityguide.Fragment;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.io.utkarsh.cityguide.R;
+
+public class MapViewFragment extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_map_view_fragment,container,false);
+        initview(view);
+        return  view;
+    }
+
+    private void initview(View view) {
+        SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
